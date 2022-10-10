63e3eeb4b70af95a20a950e68973185aab757106
Kristian Ollikainen <kristiankulta2000@gmail.com>
2022-10-10 14:07:11 +0300

    Switch to rtmidi2 - MIDI batching part 1
    mido and rtmidi packages are old and not maintained, rtmidi2 seems to be the new package to use.
    
    Implemented note-message batching for MASM side, going to do MAS side in another commit.

--- Submods/MAS Additions/MASM/scripts/midi_input.py
+++ Submods/MAS Additions/MASM/scripts/midi_input.py
@@ -1,14 +1,36 @@
-import mido
+import rtmidi2
 from socketer import MASM
 
 inPort = None
 doReadInput = False
+batched = []
+
+def MIDI_Callback(msg, timestamp):
+	global inPort
+	global batched
+	global doReadInput
+	if inPort is not None:
+		msgtype, channel = rtmidi2.splitchannel(msg[0])
+		if MASM.hasDataCheck("MIDI_KEYMAPKEY"):
+			if msgtype == rtmidi2.NOTEON:
+				note = msg[1]
+				MASM.hasDataBool("MIDI_KEYMAPKEY")
+				MASM.sendData("MIDI_KEY", note)
+		elif doReadInput: # We want to clear old pending messages but not send them if input is disabled
+			if msgtype == rtmidi2.NOTEON or msgtype == rtmidi2.NOTEOFF:
+				note, velocity = msg[1], msg[2]
+				if msgtype == rtmidi2.NOTEON and velocity > 0:
+					batched.append((f"MIDI_NOTE.{note}", velocity))
+				elif msgtype == rtmidi2.NOTEOFF or velocity == 0:
+					batched.append((f"MIDI_NOTE.{note}", 0))
 
 def Start():
 	global inPort
 	try:
-		print(f"MIDI inputs: {mido.get_input_names()}")
-		inPort = mido.open_input()
+		print(f"MIDI inputs: {rtmidi2.get_in_ports()}")
+		inPort = rtmidi2.MidiIn()
+		inPort.callback = MIDI_Callback
+		inPort.open_port()
 		print(f"MIDI input open: {inPort}")
 	except Exception as e:
 		inPort = None
@@ -16,22 +38,20 @@ def Start():
 
 def Update():
 	global inPort
+	global batched
 	global doReadInput
 	if inPort is not None:
 		if doReadInput and MASM.hasDataBool("MIDI_STOP"):
 			doReadInput = False
 		elif not doReadInput and MASM.hasDataBool("MIDI_START"):
 			doReadInput = True
-		for msg in inPort.iter_pending():
-			if MASM.hasDataCheck("MIDI_KEYMAPKEY"):
-				bytes = msg.bytes()
-				if len(bytes) >= 3:
-					MASM.hasDataBool("MIDI_KEYMAPKEY")
-					MASM.sendData("MIDI_KEY", bytes[1])
-			elif doReadInput: # We want to clear old pending messages but not send them if input is disabled
-				bytes = msg.bytes()
-				if len(bytes) >= 3:
-					if bytes[0] == 144 and bytes[2] > 0:
-						MASM.sendData(f"MIDI_NOTE.{bytes[1]}", bytes[2])
-					elif bytes[0] == 128 or bytes[2] == 0:
-						MASM.sendData(f"MIDI_NOTE.{bytes[1]}", 0)
+		elif doReadInput and len(batched) > 0:
+			MASM.sendData("MIDI_NOTES", batched)
+			batched = []
+
+def OnQuit():
+	global inPort
+	global batched
+	if inPort is not None:
+		inPort.close_port()
+		batched = []
