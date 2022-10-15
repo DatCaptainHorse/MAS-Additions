09743865a4a54cc31a24fdd8d58e0fecf3ef2f1d
Kristian Ollikainen <kristiankulta2000@gmail.com>
2022-10-15 18:18:17 +0300

    Fix and improve workflow with other OS builds

--- MASM-Source/xmake.lua
+++ MASM-Source/xmake.lua
@@ -6,7 +6,14 @@ set_languages("c++20")
 add_requires("spdlog")
 add_requires("python 3.10.*", { configs = { shared = true } })
 
-set_installdir("build/install/")
+-- Set special installdirs for each platform, for easier GitHub Actions usage
+if is_plat("windows") then
+	set_installdir("$(buildir)/install/windows/$(arch)/")
+elseif is_plat("linux") then
+	set_installdir("$(buildir)/install/linux/$(arch)/")
+elseif is_plat("macosx") then
+	set_installdir("$(buildir)/install/macosx/$(arch)/")
+end
 
 target("MASM")
     set_kind("binary")
